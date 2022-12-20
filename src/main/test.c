6e12a1148325948543c1658b51b596b370ddd5fb
Paolo Bosetti <paolo.bosetti@unitn.it>
2022-12-20 10:59:26 +0100

    Improved test.c

--- src/main/test.c
+++ src/main/test.c
@@ -5,10 +5,11 @@
 int main(int argc, char* argv[]) {
   char mac[13];
   if (argc < 2) {
+    fprintf(stderr, "Version: %s\n", GIT_COMMIT_HASH);
     fprintf(stderr, "I need the interface name as single argument!\n");
     return -1;
   }
   get_mac_addr(mac, argv[1]);
-  printf("Version: %s\nMAC address: %s\n", GIT_COMMIT_HASH, mac);
+  printf("MAC address: %s\n", mac);
   return 0;
 }
